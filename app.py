99659542aca0f2a0f326413f399262df6f1dd0dd
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-01 09:51:12 +0100

    choose between audio and video

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect
 from download_video import download_video
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hello"
 
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         link = request.form.get("link")
-        print(link)
-        if not link:
-            flash("Please fill the link fiels !", category="error")
-        result = download_video(link)
+        file_type = request.form.get("file_type")
+        print(file_type)
+        if not link or not file_type:
+            flash("Please fill all fields !", category="error")
+            return redirect("/")
+            
+        result = download_video(link, file_type)
         if not result:
             flash("Error with the link !", category="error")
+            return redirect("/")
+            
         flash("Video downloaded !", category="success")
-        
+        return redirect("/")
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
--- download_video.py
+++ download_video.py
@@ -1,14 +1,21 @@
 from pytube import *
 from pytube.exceptions import RegexMatchError
 
-def download_video(link):
+def download_video(link, file_type):
     try:
         yt = YouTube(link)
     except RegexMatchError:
         return False
     else:
-        streams = yt.streams.filter(progressive=True)
-        itag = streams[0].itag
-        stream = yt.streams.get_highest_resolution()
-        stream.download("./videos")
-        return True
+        if file_type == "video":
+            
+            streams = yt.streams.filter(progressive=True)
+            itag = streams[0].itag
+            stream = yt.streams.get_highest_resolution()
+            stream.download("./videos")
+            return True
+        elif file_type == "audio":
+            stream = yt.streams.get_audio_only()
+            print(stream)
+            stream.download("./audios")
+            return True
